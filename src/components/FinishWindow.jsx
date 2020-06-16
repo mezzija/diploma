a6cc2019976039f31d839102dafdd151b4be86d5
mezzija <mezzija@mail.ru>
2020-06-16 13:16:19 +0300

    add FinishWindow

---
+++ src/components/FinishWindow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//style
+import '../styles/components/FinishWindow.css'
+
+const FinishWindow=()=>{
+    return(
+        <div className="pyro">
+            <div className="before"/>
+            <div className="after"/>
+        </div>
+    )
+}
+export default FinishWindow;
